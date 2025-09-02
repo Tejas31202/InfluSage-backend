be521b558493b20f861f9aa89e363ddec7fbd6a0
bhaumikraval <dev1@dbmares.com>
2025-09-02 18:18:09 +0530

    restricting access to browse campaign routes.

--- src/routes/influencerroutes/InfluencerCampaignRoutes.js
+++ src/routes/influencerroutes/InfluencerCampaignRoutes.js
@@ -4,37 +4,37 @@ const routes = express.Router();
 
 // Import Form Campaign Controller
 import {
-    GetAllCampaign,
-    GetCampaignDetails,
-    ApplyNowCampaign,
-    GetUsersAppliedCampaigns,
-    SaveCampaign,
-    GetSaveCampaign,
-    GetSingleApplyCampaign,
-    GetUserCampaignWithDetails
+  GetAllCampaign,
+  GetCampaignDetails,
+  ApplyNowCampaign,
+  GetUsersAppliedCampaigns,
+  SaveCampaign,
+  GetSaveCampaign,
+  GetSingleApplyCampaign,
+  GetUserCampaignWithDetails
 } from '../../controller/influencercontroller/InfluencerCampaignController.js'
 import authenticateUser from '../../middleware/AuthMiddleware.js';
-import {upload} from "../../middleware/MulterMiddleware.js"
+import { upload } from "../../middleware/MulterMiddleware.js"
 
 
 
 //Routes For Campaign
-routes.get("/browse",GetAllCampaign);
+routes.get("/browse",authenticateUser(), GetAllCampaign);
 
 routes.get("/browse/campaign/:campaignId", GetCampaignDetails);
 
-routes.post('/apply/:campaignId', authenticateUser(["Influencer"]),upload.fields([
-    { name: "portfolioFiles", maxCount: 5}
-  ]),ApplyNowCampaign); 
+routes.post('/apply/:campaignId', authenticateUser(["Influencer"]), upload.fields([
+  { name: "portfolioFiles", maxCount: 5 }
+]), ApplyNowCampaign);
 
 routes.get('/applied/:userId', GetUsersAppliedCampaigns);
 
-routes.get("/apply/:campaignId",authenticateUser(["Influencer"]),GetSingleApplyCampaign)
+routes.get("/apply/:campaignId", authenticateUser(["Influencer"]), GetSingleApplyCampaign)
 
 routes.post('/saved/campaign', SaveCampaign);
 
-routes.get('/saved/campaign/:userId',GetSaveCampaign)
+routes.get('/saved/campaign/:userId', GetSaveCampaign)
 
-routes.get('/campaign/:campaignId/details',authenticateUser(["Influencer"]),GetUserCampaignWithDetails)
+routes.get('/campaign/:campaignId/details', authenticateUser(["Influencer"]), GetUserCampaignWithDetails)
 
 export default routes;
