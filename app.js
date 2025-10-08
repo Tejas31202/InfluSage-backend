b4bd7aacab3833fc42db980ecac3dbe26b30b430
unknown <dev2@dbmares.com>
2025-10-08 16:59:19 +0530

    edit for deploy,2

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const onlineUsers = new Map();
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT", "DELETE", "OPTIONS"],
     credentials: true,
   },
 });
