069526e01249c453ce92aa1011c3a53424568f31
bhaumikraval <dev1@dbmares.com>
2025-10-08 11:38:55 +0530

    Comment Remove

--- src/controller/NotificationController.js
+++ src/controller/NotificationController.js
@@ -1,59 +1,6 @@
 import { client } from '../config/Db.js';
 
 
-// export const insertNotification = async (req, res) => {
-
-//     const {
-//         p_notificationtypeid,
-//         p_senderid,
-//         p_receiverid,
-//         p_campaignid,
-//         p_status,
-//         p_message
-//     } = req.body;
-
-//     if (!p_notificationtypeid || !p_senderid || !p_receiverid) {
-
-//         return res.status(400).json({ message: "NotificationId and SenderId and ReceiverId Require.." })
-//     }
-//     try {
-//         const Result = await client.query
-//             (` call ins.usp_insert_notifications
-//             (
-//             $1::SMALLINT,
-//             $2::bigint,
-//             $3::bigint,
-//             $4::bigint,
-//             $5::boolean,
-//             $6::text
-//             )`,
-//                 [p_notificationtypeid,
-//                     p_senderid,
-//                     p_receiverid,
-//                     p_campaignid,
-//                     null,
-//                     null
-//                 ]
-//             )
-//         console.log(Result.rows[0]);
-//         const { p_status, p_message } = Result.rows[0];
-//         if (p_status) {
-//             return res.status(200).json({ message: p_message });
-//         } else {
-//             return res.status(400).json({ message: p_message });
-//         }
-//         return res.status(200).json({ message: p_message });
-//         // io.to(p_receiverid.toString()).emit('new_notification', {
-//         //     message: p_message,
-//         //     notificationTypeId: p_notificationtypeid,
-//         //     campaignId: p_campaignid,
-//         //     unreadCount: Number(unreadCount)
-//         // });
-//     } catch (error) {
-//         console.error('Error Create A Notification.')
-//         return res.status(500).json({ message: "Internal Server Error" })
-//     }
-// };
 
 //..................Get All Notification List............................
 export const getallNotification = async (req, res) => {
