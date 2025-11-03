a8a04e62a11327c97bf124babb9d9fcd4c0921a2
unknown <dev2@dbmares.com>
2025-11-03 16:53:48 +0530

    1st commit in test for fixing socket connect problem

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const onlineUsers = new Map();
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
