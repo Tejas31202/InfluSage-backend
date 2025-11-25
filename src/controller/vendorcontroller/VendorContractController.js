dc7b3a4c788238d129b40ac64f9c56fd9bbad7ac
miralee439 <miralee.dbmares@gmail.com>
2025-11-25 12:07:28 +0530

    add new function influencerApproveOrRejectContract

--- src/controller/vendorcontroller/VendorContractController.js
+++ src/controller/vendorcontroller/VendorContractController.js
@@ -6,7 +6,7 @@ export const getAllSelectedInfluencer = async (req, res) => {
 
   if (!campaign_id || !vendor_id) {
     return res.status(400).json({
-      error: "campaign_id and vendor_id are required",
+      message: "campaign_id and vendor_id are required",
     });
   }
   try {
@@ -116,3 +116,47 @@ export const getContractDetailByContractId = async (req,res)=>{
     });
   }
 }
+
+export const influencerApproveOrRejectContract = async (req, res) => {
+  try {
+    const p_influencerid = req.user?.id || req.body.p_influencerid;
+    const { p_contractid, p_statusname } = req.body;
+
+    if (!p_influencerid) {
+      return res.status(400).json({ message: "p_influencerid is required" });
+    }
+
+    if (!p_contractid || !p_statusname) {
+      return res
+        .status(400)
+        .json({ message: "p_contractid and p_statusname are required" });
+    }
+
+    const result = await client.query(
+      `CALL ins.usp_update_contractstatus(
+      $1::bigint,
+      $2::bigint,
+      $3::varchar(15),
+      $4::boolean,
+      $5::text
+      );`,
+      [p_influencerid, p_contractid, p_statusname, null, null]
+    );
+    
+    const { p_status, p_message } = result.rows[0];
+
+    if (p_status) {
+      return res.status(200).json({
+        message: p_message,
+        p_status,
+      });
+    } else {
+      return res.status(400).json({ message: p_message, p_status });
+    }
+  } catch (error) {
+    console.error("error in createOrEditContract:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- src/routes/vendorroutes/VendorContractRoutes.js
+++ src/routes/vendorroutes/VendorContractRoutes.js
@@ -1,5 +1,5 @@
 import express from'express';
-import {getAllSelectedInfluencer,createOrEditContract,getContractDetailByContractId} from '../../controller/vendorcontroller/VendorContractController.js'
+import {getAllSelectedInfluencer,createOrEditContract,getContractDetailByContractId,influencerApproveOrRejectContract} from '../../controller/vendorcontroller/VendorContractController.js'
 import authenticateUser from '../../middleware/AuthMiddleware.js';
 
 const routes = express.Router();
@@ -7,6 +7,6 @@ const routes = express.Router();
 routes.get('/selected/influencer', authenticateUser(["Vendor"]),getAllSelectedInfluencer);
 routes.post("/create-or-edit/contract",authenticateUser(["Vendor"]),createOrEditContract);
 routes.get("/contract-detail/:p_contractid",authenticateUser(["Vendor"]),getContractDetailByContractId);
-
+routes.post("/influencer/contract/approve-reject",authenticateUser(["Influencer"]),influencerApproveOrRejectContract);
 export default routes;
 
